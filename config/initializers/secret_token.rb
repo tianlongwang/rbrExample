153eefd14c1fb0cc133e23db56128536f50dd007
Tian Long Wang <TianLongWang@dynamic-oit-vapornet-c-1920.princeton.edu>
2012-06-08 15:37:55 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TutorialByExample::Application.config.secret_token = 'f5c2a695a97e9df73e0bca63712b8d4b172dc47f7af3249a2999e705055e069f61a52ccf086bb2e901d5bbc1bc3f8eea8ec195f920777d177b2f87f967613205'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+TutorialByExample::Application.config.session_store :cookie_store, :key => '_TutorialByExample_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# TutorialByExample::Application.config.session_store :active_record_store
